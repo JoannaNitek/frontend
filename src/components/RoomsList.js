68734690db37044f69ea02c8cff0648c6ca69cb3
Joanna <joanna.nitek@gmail.com>
2023-02-10 13:25:00 +0100

    first commit

---
+++ src/components/RoomsList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ListGroup from "react-bootstrap/ListGroup"
+
+
+export default function RoomsList() {
+    return (
+        <ListGroup>
+            <ListGroup.Item disabled>Cras justo odio</ListGroup.Item>
+            <ListGroup.Item variant="info">Dapibus ac facilisis in</ListGroup.Item>
+            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+        </ListGroup>
+    );
+}
+
